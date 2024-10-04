b3727e3afe4ccab6d28e8bcde4c72d63b85de56c
Boris Hatala <xhatal02@vutbr.cz>
2024-10-04 13:29:39 +0200

    IApiRepositories.cs fixed missdelete

--- SharpForms/SharpForms.Api.DAL.Common/Repositories/IApiRepositories.cs
+++ SharpForms/SharpForms.Api.DAL.Common/Repositories/IApiRepositories.cs
@@ -7,6 +7,11 @@ namespace SharpForms.Api.DAL.Common.Repositories
     public interface IApiRepository<TEntity>
         where TEntity : IEntity
     {
-        
+        IList<TEntity> GetAll();
+        TEntity? GetById(Guid id);
+        Guid Insert(TEntity entity);
+        Guid? Update(TEntity entity);
+        void Remove(Guid id);
+        bool Exists(Guid id);
     }
 }
